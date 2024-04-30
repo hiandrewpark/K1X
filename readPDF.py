9ca92f11cfb5f04b39d37e84d48902aa4c7a7e40
Junseong Park <hiandrew.park@gmail.com>
2024-04-30 00:42:04 -0500

    if the line contains more than one number

--- readPDF.py
+++ readPDF.py
@@ -7,13 +7,16 @@ number_lines = []
 with pdfplumber.open(pdf_path) as pdf:
     for page in pdf.pages:
         text = page.extract_text()
-        for line in text.split('\n'):
-            # If the names of the line item is no longer "Line" but still follows the general rule of "Name x. ####"
-            print(line)
-            found_numbers = re.findall(r'^.*?\d+\.\s*(\d+)$', line) # If it no longer follows the general rule above and has a mix of string and number in the values
-            if found_numbers:
-                number_lines.extend(map(int, found_numbers))
+        if text:
+            for line in text.split('\n'):
+                # If the names of the line item is no longer "Line" but still follows the general rule of "Name x. ####"
+                print(line)
+
+                # If it no longer follows the general rule above and has a mix of string and number in the values
+                # If there are multiple numbers in one line
+                found_numbers = re.findall(r'\b\d+\b', line)  
+                if found_numbers:
+                    number_lines.extend(map(int, found_numbers[1:]))
 
 total_sum = sum(number_lines)
-
 print("\nTotal sum of values in PDF: " + str(total_sum))
