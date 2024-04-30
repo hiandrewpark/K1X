8f9baeff0a6a04df81f3c5cf55bbb54fa9183fc8
Junseong Park <hiandrew.park@gmail.com>
2024-04-30 00:07:12 -0500

    line item no longer starts with 'line'

--- readPDF.py
+++ readPDF.py
@@ -1,19 +1,17 @@
 import pdfplumber
+import re
 
-pdf_path = r'C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\filled_form_2.pdf'
+pdf_path = r'C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document.pdf'
 
 number_lines = []
 with pdfplumber.open(pdf_path) as pdf:
     for page in pdf.pages:
         text = page.extract_text()
         for line in text.split('\n'):
-            if line.lower().startswith('line'):
-                line_parts = line.split('.')
-                line_number = line_parts[0].split(' ')[1]
-                line_content = line_parts[1].strip()
-
-                number_lines.append(line_content)
-                print(f'Line {line_number}: {line_content}')
+            # If the names of the line item is no longer "Line" but still follows the general rule of "Name x. ####"
+            found_numbers = re.findall(r'\d+', line)
+            if found_numbers:
+                number_lines.extend(map(int, found_numbers[1:]))
 
 numbers = [int(x) for x in number_lines]
 total_sum = sum(numbers)
