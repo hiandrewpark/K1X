e7e539b4fdfa7f371acee6a1a895f1354961a9b7
Junseong Park <hiandrew.park@gmail.com>
2024-05-01 03:57:42 -0500

    change local path

--- readPDF_test.py
+++ readPDF_test.py
@@ -2,37 +2,37 @@ from readPDF import *
 
 # Simple PDF without any issues
 def test_simple():
-    pdf_path = r"C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document_1.pdf"
+    pdf_path = r"WRITE LOCAL PATH HERE"
     total_sum = read_and_sum("Test Simple PDF", pdf_path)
     assert total_sum == 179, "test_simple failing"
 
 # If the line no longer starts with 'line'
 def test_different_line_name():
-    pdf_path = r"C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document_2.pdf"
+    pdf_path = r"WRITE LOCAL PATH HERE"
     total_sum = read_and_sum("Test Different Line Name", pdf_path)
     assert total_sum == 179, "test_different_line_name failing"
 
 # If the numeric value contains a string component
 def test_non_numeric_values():
-    pdf_path = r"C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document_3.pdf"
+    pdf_path = r"WRITE LOCAL PATH HERE"
     total_sum = read_and_sum("Test Non-Numeric Values After 'Line'", pdf_path)
     assert total_sum == 109, "test_non_numeric_values failing"
 
 # If there are multiple numbers in one line
 def test_multiple_numbers():
-    pdf_path = r"C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document_4.pdf"
+    pdf_path = r"WRITE LOCAL PATH HERE"
     total_sum = read_and_sum("Test Multiple Numbers in One Line", pdf_path)
     assert total_sum == 114, "test_multiple_numbers failing"
 
 # All test mixed in one
 def test_no_line_number():
-    pdf_path = r"C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document_5.pdf"
+    pdf_path = r"WRITE LOCAL PATH HERE"
     total_sum = read_and_sum("Test with No Line Number", pdf_path)
     assert total_sum == 116, "test_no_line_number failing"
 
 # All test mixed in one
 def test_all():
-    pdf_path = r"C:\Users\hiand\Documents\UChicago\Interview\K1X\Files\Test_Document_6.pdf"
+    pdf_path = r"WRITE LOCAL PATH HERE"
     total_sum = read_and_sum("Test All", pdf_path)
     assert total_sum == 116, "test_all failing"
 
